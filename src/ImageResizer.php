3d5ee3f10efb615b6a6641463495699352623d8a
root <root@AB9EFA7>
2020-01-20 11:48:59 +0000

    well... this was the version with working big images

--- src/ImageResizer.php
+++ src/ImageResizer.php
@@ -1,54 +1,50 @@
-<?php 
+<?php
 
 namespace PhpMediaCache;
 
-
 class ImageResizer
 {
-  public function createCachedImage($imageFileName, $flySpec)
-  {
-    $imagick = new \Imagick(); 
-    $imagick->readImage($imageFileName);
-    
-    $this->autoRotateImage($imagick);
-
-    if ($flySpec->isOriginalSize($imagick))
+    public function createCachedImage($imageFileName, $flySpec)
     {
-      $imagick->resampleImage(100, 100, \Imagick::FILTER_LANCZOS, 1);
-    } 
-    else
-    {
-      $imagick->resizeImage($flySpec->width, $flySpec->height, \Imagick::FILTER_LANCZOS, 1, true);
-    }   
-         
-    $targetFileName = tempnam('/tmp','flyfiles');  
-    $imagick->writeImage($targetFileName);
+        $imagick = new \Imagick();
+        $imagick->setResourceLimit(\imagick::RESOURCETYPE_MEMORY, 67108864);
+        $imagick->setResourceLimit(\imagick::RESOURCETYPE_MAP, 67108864);
 
-    return $targetFileName;
-  }
-  
-  protected function autoRotateImage($image) { 
-      $orientation = $image->getImageOrientation(); 
-  
-      switch($orientation) { 
-          case \imagick::ORIENTATION_BOTTOMRIGHT: 
-              $image->rotateimage("#000", 180); // rotate 180 degrees 
-          break; 
-  
-          case \imagick::ORIENTATION_RIGHTTOP: 
-              $image->rotateimage("#000", 90); // rotate 90 degrees CW 
-          break; 
-  
-          case \imagick::ORIENTATION_LEFTBOTTOM: 
-              $image->rotateimage("#000", -90); // rotate 90 degrees CCW 
-          break; 
-      } 
-  
-      // Now that it's auto-rotated, make sure the EXIF data is correct in case the EXIF gets saved with the image! 
-      $image->setImageOrientation(\imagick::ORIENTATION_TOPLEFT); 
-  }   
+        $imagick->readImage($imageFileName);
+
+        $this->autoRotateImage($imagick);
+
+        if ($flySpec->isOriginalSize($imagick)) {
+            $imagick->resampleImage(100, 100, \Imagick::FILTER_LANCZOS, 1);
+        } else {
+            $imagick->resizeImage($flySpec->width, $flySpec->height, \Imagick::FILTER_LANCZOS, 1, true);
+        }
+
+        $targetFileName = tempnam('/tmp', 'flyfiles');
+        $imagick->writeImage($targetFileName);
+
+        return $targetFileName;
+    }
+
+    protected function autoRotateImage($image)
+    {
+        $orientation = $image->getImageOrientation();
+
+        switch ($orientation) {
+          case \imagick::ORIENTATION_BOTTOMRIGHT:
+              $image->rotateimage('#000', 180); // rotate 180 degrees
+          break;
+
+          case \imagick::ORIENTATION_RIGHTTOP:
+              $image->rotateimage('#000', 90); // rotate 90 degrees CW
+          break;
+
+          case \imagick::ORIENTATION_LEFTBOTTOM:
+              $image->rotateimage('#000', -90); // rotate 90 degrees CCW
+          break;
+      }
+
+        // Now that it's auto-rotated, make sure the EXIF data is correct in case the EXIF gets saved with the image!
+        $image->setImageOrientation(\imagick::ORIENTATION_TOPLEFT);
+    }
 }
-
-
-
-
--- src/PdfToImageConverter.php
+++ src/PdfToImageConverter.php
@@ -7,22 +7,20 @@ class PdfToImageConverter
     public function createCachedImage($pdfFileName, $flySpec)
     {
         $im = new \Imagick();
-        $im->setResolution(300, 300);
+        $im->setResolution(150, 150);
         $im->readImage($pdfFileName);
         $im->resetIterator();
 
         $im = $im->appendImages(true);
 
-        $im->setImageFormat('jpg');
-
         $im->setImageBackgroundColor('white');
         $im->setImageAlphaChannel(\Imagick::ALPHACHANNEL_REMOVE);
 
-        if ($flySpec->isOriginalSize($im)) {
-            $im->resampleImage(100, 100, \Imagick::FILTER_LANCZOS, 1);
-        } else {
+        if (!$flySpec->isOriginalSize($im)) {
             $im->resizeImage($flySpec->width, $flySpec->height, \Imagick::FILTER_LANCZOS, 1, true);
         }
+
+        $im->setImageFormat('jpg');
 
         $targetFileName = tempnam('/tmp', 'flyfiles');
         $im->writeImages($targetFileName, true);
